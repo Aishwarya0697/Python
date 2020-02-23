5a506f1c1fd521a78707b96d6f692d850ab4135a
Aishwarya0697 <61375690+Aishwarya0697@users.noreply.github.com>
2020-02-23 14:46:51 +0530

    Add files via upload

---
+++ 23 2 20 .py
@@ -0,0 +1,42 @@
+a=int(input('enter input 1 ' ))
+b=int(input('enter input 2 ' ))
+print('adition is ',a+b)
+
+# if(a==b):
+#     print('both are same')
+# else:
+#     print('both are not same')
+# if(a>=b):
+#     print('both are same')
+# else:
+#     print('both are not same')
+#
+# if(a and b):
+#     print('both are greater than zero ')
+# else:
+#     print('one is less than zero')
+
+# c=a & b
+# print('binary and ',c)
+
+# c=(a|b)
+# print('binary or',c)
+
+# c=(a^b)
+# print('binary xor',c)
+
+# c=~b
+# print('binary not',c)
+
+c=(a<<b)
+print('binary left shift',c)
+
+c=(a>>b)
+print('binary right shift',c)            # b is a value how many times shift to left or right
+
+
+
+
+
+
+
